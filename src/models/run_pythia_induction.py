5aecf675dfda3d69399ae70030240b5ca37ae4b9
Sean <trott.sean@gmail.com>
2026-05-27 11:08:20 -0400

    tryihg

--- src/models/run_pythia_induction.py
+++ src/models/run_pythia_induction.py
@@ -25,7 +25,7 @@ MODELS = [
     'EleutherAI/pythia-410m',
     'EleutherAI/pythia-1b',
     'EleutherAI/pythia-1.4b',
-    'EleutherAI/pythia-2.8b',
+    # 'EleutherAI/pythia-2.8b',
     'EleutherAI/pythia-6.9b',
     'EleutherAI/pythia-12b',
 ]
