bf5434d837c9df347380bd6b6f13da53e8b0d235
Sean <trott.sean@gmail.com>
2026-05-27 14:14:14 -0400

    update

--- src/processing/process_attns.py
+++ src/processing/process_attns.py
@@ -37,7 +37,8 @@ for filepath in tqdm(csv_files):
                   mean_prev_self_ratio=("prev_to_self_ratio", "mean"),
                   mean_prev_all_ratio=("prev_to_all_ratio", "mean"),
                   prev_fraction=("prev_token_fraction", "mean"),
-                  mean_1back=("1-back attention", "mean")
+                  mean_1back=("1-back attention", "mean"),
+                  mean_first=("first_token", "mean")
               )
               .reset_index()
         )
